9f5eec4c842efd479615d5c698442526769d4ded
Lukas Turcani <lukasturcani93@gmail.com>
2024-07-10 15:03:11 +0200

    wip

--- end/top_peaks.py
+++ end/top_peaks.py
@@ -8,9 +8,10 @@ def main() -> None:
     args = parse_args()
     connection = sqlite3.connect(args.database)
     cages = pl.read_database("SELECT * FROM cages", connection)
+
     imine_peaks = pl.read_database("SELECT * FROM imine_peaks", connection)
     imine_peaks = cages.join(imine_peaks, on="cage_id")
-    top_peaks = (
+    imine_top_peaks = (
         (
             imine_peaks.group_by("cage_id").agg(
                 pl.col("peak_id").top_k_by("intensity", 2)
@@ -19,8 +20,20 @@ def main() -> None:
         .explode("peak_id")
         .join(imine_peaks, on="peak_id")
     )
+    print(imine_top_peaks)
 
-    print(top_peaks)
+    aldehyde_peaks = pl.read_database("SELECT * FROM aldehyde_peaks", connection)
+    aldehyde_peaks = cages.join(aldehyde_peaks, on="cage_id")
+    aldehyde_top_peaks = (
+        (
+            aldehyde_peaks.group_by("cage_id").agg(
+                pl.col("peak_id").top_k_by("intensity", 2)
+            )
+        )
+        .explode("peak_id")
+        .join(aldehyde_peaks, on="peak_id")
+    )
+    print(aldehyde_top_peaks)
 
 
 def parse_args() -> argparse.Namespace:
